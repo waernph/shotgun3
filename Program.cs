d6b2670be5b64bcd1207c490f67d750cb81eeb16
Philip Waern <waernph@gmail.com>
2025-09-19 10:13:24 +0200

    Loopen för att starta om är på plats och fungerar. Spelet är nog klart. Kanske tillkommer funktioner så som sätta eget namn, generera namn åt datorn låta två personer spela mot varandra.

--- Program.cs
+++ Program.cs
@@ -2,25 +2,58 @@
 {
     public static void Main()
     {
-        Player player1 = new Player(true);
-        Player player2 = new Player(false);
-        Graphics gfx = new Graphics();
-        bool gameOn = true;
-
-        while (gameOn)
+        bool restart = true;
+        while (restart)
         {
-            Console.Clear();
-            gfx.ShotgunLogo();
-            Console.WriteLine(
-                $"Spelare 1: {player1.DisplayPlayersShots()} skott   |   Spelare 2: {player2.DisplayPlayersShots()} skott"
-            );
-            int player1Choice = player1.PlayerChoice();
-            int player2Choice = player2.PlayerChoice();
-            string winner = Winner.WinnerIs(player1Choice, player2Choice);
-            if (winner == "Spelare 1" || winner == "Spelare 2")
+            Player player1 = new Player(true);
+            Player player2 = new Player(false);
+            bool gameOn = true;
+            bool validInput = false;
+            char userInput;
+            while (gameOn)
             {
-                Console.WriteLine($"\n{winner} vann!");
-                gameOn = false;
+                Console.Clear();
+                Graphics.ShotgunLogo();
+                Console.WriteLine(
+                    $"Spelare 1: {player1.DisplayPlayersShots()} skott   |   Spelare 2: {player2.DisplayPlayersShots()} skott"
+                );
+                int player1Choice = player1.PlayerChoice();
+                int player2Choice = player2.PlayerChoice();
+                string winner = Winner.WinnerIs(player1Choice, player2Choice);
+                if (winner == "Spelare 1" || winner == "Spelare 2")
+                {
+                    string weapon = "";
+                    if (player1Choice == 1 || player2Choice == 1)
+                    {
+                        weapon = "pistol";
+                    }
+                    if (player1Choice == 4 || player2Choice == 4)
+                    {
+                        weapon = "hagelgevär";
+                    }
+
+                    Console.WriteLine($"\n{winner} vann med {weapon}!");
+                    while (!validInput)
+                    {
+                        Console.WriteLine("\nVill du spela igen? Y/N");
+                        userInput = char.ToLower(Console.ReadKey().KeyChar);
+                        if (userInput == 'y')
+                        {
+                            restart = true;
+                            validInput = true;
+                        }
+                        else if (userInput == 'n')
+                        {
+                            restart = false;
+                            validInput = true;
+                        }
+                        else
+                        {
+                            validInput = false;
+                        }
+                    }
+                    gameOn = false;
+                }
             }
         }
     }
