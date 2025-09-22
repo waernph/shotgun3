94740f4f36ce6b799829f7f14f96368dd1849779
waernph <102241220+waernph@users.noreply.github.com>
2025-09-22 15:08:38 +0200

    Småändrignar

--- Graphics.cs
+++ Graphics.cs
@@ -21,4 +21,19 @@ class Graphics
             $"Spelare 1: {player1.DisplayPlayersShots()} skott   |   Spelare 2: {player2.DisplayPlayersShots()} skott"
         );
     }
+
+    public static void NotValidInput(int choice)
+    {
+        choice -= 100;
+        string[] errorMessage =
+        [
+            "Du har inga skott, försök inte!",
+            "Valet finns inte. Försök igen",
+            "Du måste ha minst 3 skott",
+        ];
+        //Console.WriteLine(choice);
+        Console.WriteLine("\n" + errorMessage[choice]);
+        Thread.Sleep(2000);
+        //Console.ReadKey();
+    }
 }
