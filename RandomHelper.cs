d19f07595fba9972e32a6f5f744707830da52223
Philip Waern <waernph@gmail.com>
2025-09-25 09:47:05 +0200

    Ändrade struktur på random för att det ska se lite enklare ut

--- RandomHelper.cs
+++ RandomHelper.cs
@@ -1,14 +1,29 @@
 static class RandomHelper
 {
-    private static Random _rnd = new Random();
+    private static Random rnd = new Random();
 
-    public static int ComputerRandomInterval23() => _rnd.Next(2, 3);
+    public static int ComputerRandomInterval23()
+    {
+        return rnd.Next(2, 3);
+    }
 
-    public static int ComputerRandomInterval05() => _rnd.Next(0, 5);
+    public static int ComputerRandomInterval05()
+    {
+        return rnd.Next(0, 5);
+    }
 
-    public static int ComputerRandomInterval14() => _rnd.Next(1, 4);
+    public static int ComputerRandomInterval14()
+    {
+        return rnd.Next(1, 4);
+    }
 
-    public static int ComputerRandomInterval59() => _rnd.Next(5, 9);
+    public static int ComputerRandomInterval59()
+    {
+        return rnd.Next(5, 9);
+    }
 
-    public static int WinnerRandom12() => _rnd.Next(1, 2);
+    public static int WinnerRandom12()
+    {
+        return rnd.Next(1, 2);
+    }
 }
